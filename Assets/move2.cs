a3fd58d37b95be7b5951bd792cade1d4288ae861
jordannstrong <jordannstrong@gmail.com>
2017-11-11 14:55:35 -0500

    Scripts that rotates ship to point at mouse, camera follows ship

---
+++ Assets/move2.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move2 : MonoBehaviour
+{
+	public enum MoveType
+	{
+		worldAxis,
+		localAxis
+	}
+	public MoveType moveType;
+	public float moveSpeed = 1.0f;
+	public float rotationSpeedOnLocalAxis = 15.0f;
+	float angle;
+	float x;
+	float z;
+
+	void Awake()
+	{
+		StartCoroutine ("ChildManager");
+	}
+
+	void Update ()
+	{
+		//
+		x = Input.GetAxis ("Horizontal")*moveSpeed;
+		z = Input.GetAxis ("Vertical")*moveSpeed;
+		if(moveType == MoveType.worldAxis)
+		{
+			transform.position = new Vector3 (transform.position.x+x, transform.position.y, transform.position.z+z);
+		}
+		else
+		{
+			transform.Rotate(transform.up,x*rotationSpeedOnLocalAxis);
+			transform.Translate(0.0f,0.0f,z*100.0f*Time.deltaTime,transform);
+		}
+
+	}
+
+	IEnumerator ChildManager ()
+	{
+		GameObject go = new GameObject("follower");
+		go.transform.position = transform.position - new Vector3 (transform.position.x, transform.position.y, transform.position.z - 1.0f);
+		for(;;)
+		{
+			go.transform.position = new Vector3(Mathf.Lerp(go.transform.position.x,transform.position.x,moveSpeed*100.0f*Time.deltaTime),
+				Mathf.Lerp(go.transform.position.y,transform.position.y,moveSpeed*100.0f*Time.deltaTime),
+				Mathf.Lerp(go.transform.position.z,transform.position.z,moveSpeed*100.0f*Time.deltaTime));
+			angle = Vector3.Angle(go.transform.position,transform.position);
+			Debug.DrawLine(go.transform.position,transform.position);
+			if(moveType == MoveType.worldAxis)
+			{
+				transform.LookAt(go.transform);
+			}
+			yield return null;
+		}
+	}
+}
---
+++ Assets/target.js
@@ -0,0 +1,22 @@
+﻿var target : Transform;
+var maxParticles : int = 15;
+var rotationVelocity = 2;
+private var particles : ParticleSystem.Particle[];
+
+function Start () {
+    target = transform.parent;
+    particles = new ParticleSystem.Particle[maxParticles];
+}
+
+function Update () {
+
+    GetComponent.<ParticleSystem>().startRotation = target.rotation.eulerAngles.y * 1 * Mathf.Deg2Rad;
+
+    var count:int = GetComponent.<ParticleSystem>().GetParticles(particles);
+
+    for(var i : int = 0; i <= count; i++)
+    {
+            particles[i].rotation = Mathf.LerpAngle(particles[i].rotation, target.rotation.eulerAngles.y * 1, Time.deltaTime * rotationVelocity);
+            GetComponent.<ParticleSystem>().SetParticles(particles, count);
+    }      
+}

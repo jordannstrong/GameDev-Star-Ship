627cffd081fab9b7768125660ca60c792bb8361f
Ryan Lindsay <lindsa48@students.rowan.edu>
2017-12-01 11:49:59 -0500

    Different Shots
    Created three different shot options: single, double, triple
    Still need to implement pickups to change shot type

--- Assets/Scripts/Done_PlayerController.cs
+++ Assets/Scripts/Done_PlayerController.cs
@@ -2,43 +2,58 @@
 using System.Collections;
 
 [System.Serializable]
-public class Done_Boundary 
-{
+public class Done_Boundary {
 	public float xMin, xMax, zMin, zMax;
 }
 
-public class Done_PlayerController : MonoBehaviour
-{
+public class Done_PlayerController : MonoBehaviour {
 	public float speed;
 	public float tilt;
 	public Done_Boundary boundary;
 
 	public GameObject shot;
-	public Transform shotSpawn;
+	public Transform[] shotSpawners;
 	public float fireRate;
-	 
+
+	public int shotType;
 	private float nextFire;
 	
-	void Update ()
-	{
-		if (Input.GetButton("Fire1") && Time.time > nextFire) 
-		{
+	void Update () {
+		if (Input.GetButton("Fire1") && Time.time > nextFire) {
 			nextFire = Time.time + fireRate;
-			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
-			GetComponent<AudioSource>().Play ();
+
+			switch (shotType) {
+			case 2: // triple shot
+				foreach (Transform spawner in shotSpawners) {
+					if (spawner.tag.Equals ("Triple Shot"))
+						Instantiate (shot, spawner.position, spawner.rotation);
+				}
+				break;
+			case 1: // double shot
+				foreach (Transform spawner in shotSpawners) {
+					if (spawner.tag.Equals ("Double Shot"))
+						Instantiate (shot, spawner.position, spawner.rotation);
+				}
+				break;
+			default: // single shot
+				foreach (Transform spawner in shotSpawners) {
+					if (spawner.tag.Equals ("Single Shot"))
+						Instantiate (shot, spawner.position, spawner.rotation);
+				}
+				break;
+			}
+			GetComponent<AudioSource> ().Play ();
 		}
 	}
 
-	void FixedUpdate ()
-	{
+	void FixedUpdate ()	{
 		float moveHorizontal = Input.GetAxis ("Vertical");
 		float moveVertical = Input.GetAxis ("Horizontal");
 
 		Vector3 movement = new Vector3 (-moveHorizontal, 0.0f, moveVertical);
 		GetComponent<Rigidbody>().velocity = movement * speed;
 		
-		GetComponent<Rigidbody>().position = new Vector3
-		(
+		GetComponent<Rigidbody>().position = new Vector3 (
 			Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 
 			0.0f, 
 			Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)

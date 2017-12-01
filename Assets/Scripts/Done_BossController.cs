a0a0e2d1b5c8ab981d14d8b4dadda25c9a2bcb31
Ryan Lindsay <lindsa48@students.rowan.edu>
2017-12-01 15:13:03 -0500

    Updated Boss
    Update to boss to show that he has hitpoints and takes hits. Still need to implement him spawning

---
+++ Assets/Scripts/Done_BossController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Done_BossController : MonoBehaviour
+{
+	public GameObject explosionKill;
+	public GameObject explosionHit;
+	public int scoreValue;
+	public int hitpoints;
+	private Done_GameController gameController;
+
+	void Start () {
+		GameObject gameControllerObject = GameObject.FindGameObjectWithTag ("GameController");
+		if (gameControllerObject != null) {
+			gameController = gameControllerObject.GetComponent <Done_GameController>();
+		}
+		if (gameController == null) {
+			Debug.Log ("Cannot find 'GameController' script");
+		}
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "PlayerBolt") {
+			hitpoints--;
+			Destroy (other.gameObject);
+			if (explosionHit != null) {
+				Instantiate (explosionHit, transform.position, transform.rotation);
+			}
+		}
+
+		if (hitpoints == 0) {
+			Destroy (gameObject);
+			Destroy (other.gameObject);
+			if (explosionKill != null) {
+				Instantiate (explosionKill, transform.position, transform.rotation);
+			}
+		}
+	}
+}
